73f88c9b3c5092faf590dea8bac7930c932bdd60
yamingdeng <yamingdeng@outlook.com>
2024-12-23 12:09:43 +0800

    Permit2 signatureTransfer

--- contracts/Permit2App.sol
+++ contracts/Permit2App.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity >=0.8.0 <0.9.0;
 
+import "hardhat/console.sol";
 import { IPermit2, IAllowanceTransfer, ISignatureTransfer } from "permit2/src/interfaces/IPermit2.sol";
 
 contract Permit2App {
@@ -13,6 +14,8 @@ contract Permit2App {
 
     // errors
     error InvalidSpender();
+    error InvalidSignature();
+    error InvalidSigner();
 
     // public functions
     // Allowance Transfer. when permit has not yet been called or needs to be refreshed.
@@ -43,5 +46,71 @@ contract Permit2App {
     // Transfers the allowed tokens from user to spender (current contract)
     function _receiveUserTokens(address token, uint160 amount) internal {
         permit2.transferFrom(msg.sender, address(this), amount, token);
+    }
+
+    // Normal signature Transfer
+    function signatureTransfer(
+        address token,
+        uint256 amount,
+        uint256 nonce,
+        uint256 deadline,
+        bytes calldata signature
+    ) public {
+        //
+        ISignatureTransfer.PermitTransferFrom memory permitTransfer = ISignatureTransfer.PermitTransferFrom({
+            permitted: ISignatureTransfer.TokenPermissions({ token: token, amount: amount }),
+            nonce: nonce,
+            deadline: deadline
+        });
+        //
+        ISignatureTransfer.SignatureTransferDetails memory transferDetails = ISignatureTransfer
+            .SignatureTransferDetails({ to: address(this), requestedAmount: amount });
+        // The owner of the tokens has to be the signer
+        // signature from signing hash of permit data per EIP-712 standards
+        permit2.permitTransferFrom(permitTransfer, transferDetails, msg.sender, signature);
+    }
+
+    // witness
+    struct Witness {
+        address user;
+    }
+    string constant WITNESS_TYPE_STRING =
+        "Witness witness)TokenPermissions(address token,uint256 amount)Witness(address user)";
+    //
+    bytes32 constant WITNESS_TYPEHASH = keccak256("Witness(address user)");
+
+    // SignatureTransfer technique with extra witness data
+    function signatureTransferWithWitness(
+        address token,
+        uint256 amount,
+        uint256 nonce,
+        uint256 deadline,
+        address user, // example extra witness data
+        bytes calldata signature
+    ) public {
+        //
+        bytes32 witness = keccak256(abi.encode(WITNESS_TYPEHASH, Witness(user)));
+        try
+            permit2.permitWitnessTransferFrom(
+                ISignatureTransfer.PermitTransferFrom({
+                    permitted: ISignatureTransfer.TokenPermissions({ token: token, amount: amount }),
+                    nonce: nonce,
+                    deadline: deadline
+                }),
+                ISignatureTransfer.SignatureTransferDetails({ to: address(this), requestedAmount: amount }),
+                msg.sender, // The owner of the tokens has to be the signer
+                witness, // Extra data to include when checking the signature
+                WITNESS_TYPE_STRING, // EIP-712 type definition for REMAINING string stub of the typehash
+                signature // The resulting signature from signing hash of permit data per EIP-712 standards
+            )
+        {} catch Error(string memory err) {
+            revert(err);
+        } catch (bytes memory err) {
+            assembly {
+                // Look for revert reason and bubble it up if present
+                // skips the length field, this revert will bubble the orignal error from call/delegatecall
+                revert(add(err, 32), mload(err))
+            }
+        }
     }
 }
--- test/Permit2App.ts
+++ test/Permit2App.ts
@@ -9,6 +9,11 @@ import hre from "hardhat";
 import { ethers } from "hardhat";
 import { BaseContract, Signer } from "ethers";
 
+const {
+    AllowanceTransfer, // Useful for generating permit data for allowance style approvals
+    SignatureTransfer, // Useful for generating permit data for signature style approvals
+    PERMIT2_ADDRESS, // 0x000000000022D473030F116dDEE9F6B43aC78BA3
+    MaxAllowanceTransferAmount } = require('@uniswap/permit2-sdk');
 
 import Permit2Artifact from "../lib/permit2/out/Permit2.sol/Permit2.json";
 import { GldToken } from "../typechain-types";
@@ -18,16 +23,47 @@ const PERMIT_DETAILS = [
     { name: "amount", type: "uint160" },
     { name: "expiration", type: "uint48" },
     { name: "nonce", type: "uint48" },
-  ];
-  
-  const PERMIT_TYPES = {
+];
+
+const PERMIT_TYPES = {
     PermitSingle: [
-      { name: "details", type: "PermitDetails" },
-      { name: "spender", type: "address" },
-      { name: "sigDeadline", type: "uint256" },
+        { name: "details", type: "PermitDetails" },
+        { name: "spender", type: "address" },
+        { name: "sigDeadline", type: "uint256" },
     ],
     PermitDetails: PERMIT_DETAILS,
-  };
+};
+
+const TOKEN_PERMISSIONS = [
+    { name: 'token', type: 'address' },
+    { name: 'amount', type: 'uint256' },
+];
+
+const PERMIT_TRANSFER_FROM_TYPES = {
+    PermitTransferFrom: [
+        { name: 'permitted', type: 'TokenPermissions' },
+        { name: 'spender', type: 'address' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'deadline', type: 'uint256' },
+    ],
+    TokenPermissions: TOKEN_PERMISSIONS,
+};
+
+const PERMIT_TRANSFER_WITH_WITNESS_FROM_TYPES = {
+    PermitTransferFrom: [
+        { name: 'permitted', type: 'TokenPermissions' },
+        { name: 'spender', type: 'address' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'deadline', type: 'uint256' },
+        { name: 'witness', type: 'Witness' },
+    ],
+    TokenPermissions: TOKEN_PERMISSIONS,
+    Witness: [{ name: 'user', type: 'address' }],
+};
+
+function toDeadline(expiration: number): number {
+    return Math.floor((Date.now() + expiration) / 1000);
+}
 
 describe("Permit2App", function () {
     let [owner, user1, user2]: Signer[] = [];
@@ -69,8 +105,7 @@ describe("Permit2App", function () {
         it("Should be permit2 address", async function () {
             await expect(permit2.getAddress()).to.eventually.equal(await keeper.permit2());
         });
-        it("Should permit success given valid signature", async function () {
-            const deadline = Math.floor(Date.now() / 1000) + 60 * 60 * 24;
+        it("Should permit success given valid signature and approval", async function () {
             const nonce = 0;
             //
             const tokenAddress = await token.getAddress();
@@ -83,12 +118,12 @@ describe("Permit2App", function () {
             const permitSingle = {
                 details: {
                     token: tokenAddress,
-                    amount: amount,
-                    expiration: deadline,
+                    amount: BigInt("0xffffffffffffffffffffffffffffffffffffffff"),
+                    expiration: toDeadline(1000 * 60 * 60 * 24 * 30),
                     nonce,
                 },
                 spender: keeperAddress,
-                sigDeadline: deadline,
+                sigDeadline: toDeadline(1000 * 60 * 60 * 5),
             };
             const domain = {
                 name: "Permit2",
@@ -103,28 +138,25 @@ describe("Permit2App", function () {
             //
             const resp = await permit2.allowance(userAddress, tokenAddress, keeperAddress);
             // console.log(resp);
-            expect(resp[0]).to.be.equal(amount);
+            expect(resp[0]).to.be.equal(BigInt("0xffffffffffffffffffffffffffffffffffffffff"));
         });
-        it("Should permit and transfer success given valid signature", async function () {
-            const deadline = Math.floor(Date.now() / 1000) + 60 * 60 * 24;
+        it("Should permit and transfer success given valid signature and approval", async function () {
             const nonce = 0;
             //
             const tokenAddress = await token.getAddress();
             const permit2Address = await permit2.getAddress();
             const keeperAddress = await keeper.getAddress();
-            const userAddress = await user1.getAddress();
             //
-            // console.log(tokenAddress, permit2Address, keeperAddress, userAddress);
             //
             const permitSingle = {
                 details: {
                     token: tokenAddress,
-                    amount: amount,
-                    expiration: deadline,
+                    amount: BigInt("0xffffffffffffffffffffffffffffffffffffffff"),
+                    expiration: toDeadline(1000 * 60 * 60 * 24 * 30),
                     nonce,
                 },
                 spender: keeperAddress,
-                sigDeadline: deadline,
+                sigDeadline: toDeadline(1000 * 60 * 60 * 5),
             };
             const domain = {
                 name: "Permit2",
@@ -147,6 +179,81 @@ describe("Permit2App", function () {
             await expect(token.balanceOf(await user1.getAddress())).to.eventually.equal(transferAmount - 100 - 100);
             await expect(token.balanceOf(await keeper.getAddress())).to.eventually.equal(100 + 100);
         });
+        it("Should permit and transfer success given valid one-time signature", async function () {
+            const deadline = toDeadline(1000 * 60 * 60 * 30);
+            const nonce = Math.floor(Math.random() * 1e15);
+            //
+            const tokenAddress = await token.getAddress();
+            const permit2Address = await permit2.getAddress();
+            const keeperAddress = await keeper.getAddress();
+            //
+            const permitAmount = 100;
+            const permit = {
+                permitted: {
+                    token: tokenAddress,
+                    amount: permitAmount
+                },
+                spender: keeperAddress,
+                nonce,
+                deadline: deadline, // deadline on the signature.
+            };
+            const domain = {
+                name: "Permit2",
+                chainId: (await hre.ethers.provider.getNetwork()).chainId,
+                verifyingContract: permit2Address,
+            };
+            //
+            const signature = await user1.signTypedData(domain, PERMIT_TRANSFER_FROM_TYPES, permit);
+            // console.log("Signature:", signature);
+            // traditional approval first then transfer
+            await token.connect(user1).approve(permit2Address, permitAmount);
+            //
+            await keeper.connect(user1).signatureTransfer(tokenAddress, permitAmount, nonce, deadline, signature);
+            //
+            await expect(token.balanceOf(await user1.getAddress())).to.eventually.equal(transferAmount - permitAmount);
+            await expect(token.balanceOf(await keeper.getAddress())).to.eventually.equal(permitAmount);
+            //
+        });
+        it("Should permit and transfer success given witness and valid one-time signature", async function () {
+            const deadline = toDeadline(1000 * 60 * 60 * 30);
+            const nonce = Math.floor(Math.random() * 1e15);
+            //
+            const tokenAddress = await token.getAddress();
+            const permit2Address = await permit2.getAddress();
+            const keeperAddress = await keeper.getAddress();
+            const userAddress = await user1.getAddress();
+            //
+            const permitAmount = 100;
+            const permit = {
+                permitted: {
+                    token: tokenAddress,
+                    amount: permitAmount
+                },
+                spender: keeperAddress,
+                nonce,
+                deadline: deadline, // deadline on the signature.
+            };
+            const witness = {
+                witnessTypeName: 'Witness', // type name must match the struct name we created in Permit2App
+                witnessType: { Witness: [{ name: 'user', type: 'address' }] }, // must match structs data
+                witness: { user: userAddress }, // witness' value
+            }
+            const chainId = (await hre.ethers.provider.getNetwork()).chainId;
+            const { domain, types, values } = SignatureTransfer.getPermitData(permit, permit2Address, chainId, witness);
+            //
+            // console.log("domain: ", domain);
+            // console.log("types: ", types);
+            // console.log("values: ", values);
+            //
+            const signature = await user1.signTypedData(domain, types, values);
+            // traditional approval first then transfer
+            await token.connect(user1).approve(permit2Address, permitAmount);
+            //
+            await keeper.connect(user1).signatureTransferWithWitness(tokenAddress, permitAmount, nonce, deadline, userAddress, signature);
+            //
+            await expect(token.balanceOf(userAddress)).to.eventually.equal(transferAmount - permitAmount);
+            await expect(token.balanceOf(keeperAddress)).to.eventually.equal(permitAmount);
+            //
+        });
     });
-
 });
