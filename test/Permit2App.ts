a47b97d29e7bd7bf9868e96785393ff7d29f7339
yamingdeng <yamingdeng@outlook.com>
2024-12-23 09:32:20 +0800

    Permit2 case

--- test/Permit2App.ts
+++ test/Permit2App.ts
@@ -34,13 +34,15 @@ describe("Permit2App", function () {
     let permit2: any;
     let keeper: any;
     let token: GldToken;
+    const amount = 100_000;
+    const transferAmount = 1_000;
 
     beforeEach(async function () {
         // Contracts are deployed using the first signer/account by default
         [owner, user1, user2] = await hre.ethers.getSigners();
         //
         const TokenFactory = await hre.ethers.getContractFactory("GldToken");
-        token = await TokenFactory.deploy(100_000);
+        token = await TokenFactory.deploy(amount);
         await token.waitForDeployment();
         //
         const permitFactory = new hre.ethers.ContractFactory(
@@ -54,11 +56,12 @@ describe("Permit2App", function () {
         keeper = await Contract.deploy(await permit2.getAddress());
         await keeper.waitForDeployment();
         //
+        await token.connect(owner).transfer(await user1.getAddress(), transferAmount);
     });
 
     describe("Token", function () {
-        it("Should balance equal to 100_000", async function () {
-            await expect(token.balanceOf(await owner.getAddress())).to.eventually.equal(100_000);
+        it("Should balance equal to 1_000", async function () {
+            await expect(token.balanceOf(await user1.getAddress())).to.eventually.equal(transferAmount);
         });
     });
 
@@ -67,16 +70,15 @@ describe("Permit2App", function () {
             await expect(permit2.getAddress()).to.eventually.equal(await keeper.permit2());
         });
         it("Should permit success given valid signature", async function () {
-            const amount = 100_000;
             const deadline = Math.floor(Date.now() / 1000) + 60 * 60 * 24;
             const nonce = 0;
             //
             const tokenAddress = await token.getAddress();
             const permit2Address = await permit2.getAddress();
             const keeperAddress = await keeper.getAddress();
-            const ownerAddress = await owner.getAddress();
+            const userAddress = await user1.getAddress();
             //
-            // console.log(tokenAddress, permit2Address, keeperAddress, ownerAddress);
+            // console.log(tokenAddress, permit2Address, keeperAddress, userAddress);
             //
             const permitSingle = {
                 details: {
@@ -94,26 +96,25 @@ describe("Permit2App", function () {
                 verifyingContract: permit2Address,
             };
             //
-            const signature = await owner.signTypedData(domain, PERMIT_TYPES, permitSingle);
+            const signature = await user1.signTypedData(domain, PERMIT_TYPES, permitSingle);
             //
-            await expect(keeper.connect(owner).permitWithPermit2(permitSingle, signature))
+            await expect(keeper.connect(user1).permitWithPermit2(permitSingle, signature))
                 .not.to.be.reverted;
             //
-            const resp = await permit2.allowance(ownerAddress, tokenAddress, keeperAddress);
+            const resp = await permit2.allowance(userAddress, tokenAddress, keeperAddress);
             // console.log(resp);
             expect(resp[0]).to.be.equal(amount);
         });
         it("Should permit and transfer success given valid signature", async function () {
-            const amount = 100_000;
             const deadline = Math.floor(Date.now() / 1000) + 60 * 60 * 24;
             const nonce = 0;
             //
             const tokenAddress = await token.getAddress();
             const permit2Address = await permit2.getAddress();
             const keeperAddress = await keeper.getAddress();
-            const ownerAddress = await owner.getAddress();
+            const userAddress = await user1.getAddress();
             //
-            // console.log(tokenAddress, permit2Address, keeperAddress, ownerAddress);
+            // console.log(tokenAddress, permit2Address, keeperAddress, userAddress);
             //
             const permitSingle = {
                 details: {
@@ -131,19 +132,19 @@ describe("Permit2App", function () {
                 verifyingContract: permit2Address,
             };
             //
-            const signature = await owner.signTypedData(domain, PERMIT_TYPES, permitSingle);
+            const signature = await user1.signTypedData(domain, PERMIT_TYPES, permitSingle);
             // traditional approval first then transfer
-            await token.connect(owner).approve(permit2Address, amount);
+            await token.connect(user1).approve(permit2Address, amount);
             //
-            await keeper.connect(owner).allowanceTransferWithPermit(permitSingle, signature, 100);
+            await keeper.connect(user1).allowanceTransferWithPermit(permitSingle, signature, 100);
             //
             // console.log(resp);
-            await expect(token.balanceOf(await owner.getAddress())).to.eventually.equal(100_000 - 100);
+            await expect(token.balanceOf(await user1.getAddress())).to.eventually.equal(transferAmount - 100);
             await expect(token.balanceOf(await keeper.getAddress())).to.eventually.equal(100);
             //
-            await keeper.connect(owner).allowanceTransferWithoutPermit(tokenAddress, 100);
+            await keeper.connect(user1).allowanceTransferWithoutPermit(tokenAddress, 100);
             //
-            await expect(token.balanceOf(await owner.getAddress())).to.eventually.equal(100_000 - 100 - 100);
+            await expect(token.balanceOf(await user1.getAddress())).to.eventually.equal(transferAmount - 100 - 100);
             await expect(token.balanceOf(await keeper.getAddress())).to.eventually.equal(100 + 100);
         });
     });
