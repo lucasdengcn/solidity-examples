b8e2cffed21108885564e8f91b958c8a14efd869
yamingdeng <yamingdeng@outlook.com>
2024-12-25 11:46:38 +0800

    ReentrancyGuard

--- contracts/CookieClicker.sol
+++ contracts/CookieClicker.sol
@@ -3,15 +3,16 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/Multicall.sol";
 import "@openzeppelin/contracts/utils/Nonces.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract CookieClicker is Multicall, Nonces {
+contract CookieClicker is Multicall, Nonces, ReentrancyGuard {
     uint256 public cookies = 0;
 
-    function click() public {
+    function click() public nonReentrant {
         cookies += 1;
     }
 
-    function nonce(address owner) public {
+    function nonce(address owner) public nonReentrant {
         super._useNonce(owner);
     }
 }
